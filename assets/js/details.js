d9e617b3ba798d3969e08587de75bfe6b86d694d
fedeval98 <fede.val9806@gmail.com>
2023-11-22 06:08:26 -0300

    terminado seccion details

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,43 +1,72 @@
-import { 
-  introducirCard,
-  crearTablaDetails,
-  crearElementosDetails,
-} from './module/functions.js'
-
 //Key de la API por headers
 const requestOptions = {
   headers:{
     "x-api-key": '0ff70d54-dc0b-4262-9c3d-776cb0f34dbd'
   }
 }
-let moviesData = {}
-fetch ("https://moviestack.onrender.com/api/movies", requestOptions)
-  .then(response => response.json())
-  .then (data => {
-    moviesData = data
-    const movies = moviesData.movies
+const {createApp} = Vue
 
-const search = location.search
-
-const params = new URLSearchParams(search)
-
-const id = params.get('id')
-
-const movieid =  movies.find(movie => movie.id == id)
-
-// Contenedor de las peliculas
-const detailsContainer = document.getElementById("movieDetails")
-const tabla1 = document.getElementById("datos1")
-const tabla2 = document.getElementById("datos2")
-// Tabla 1
-const propiedadesTabla1 = ["original_language", "release_date", "runtime", "status"]
-introducirCard(movieid, detailsContainer,crearElementosDetails)
-crearTablaDetails(movieid, propiedadesTabla1, tabla1)
+const options = {
+  data(){
+    return{
+      id: null,
+      movie:[],
+    }// aca termina el return
+  }, // aca termina el data
+  beforeCreate(){
+    const search = location.search
+    const params = new URLSearchParams(search)
+    this.id = params.get('id')
+    fetch ("https://moviestack.onrender.com/api/movies/"+this.id, requestOptions)
+      .then(response => response.json())
+      .then (data => {
+        this.movie = data
+      })
+      .catch(error => console.log(`Error:${error}`))
+  }, // aca termina el beforeCreate
 
 
-// Tabla 2
-const propiedadesTabla2 = ["vote_average", "budget", "revenue"]
-crearTablaDetails(movieid, propiedadesTabla2, tabla2)
+//computed sirve para definir propiedades calculadas
+//utilizando propiedades reactivas
+  computed:{
+    formatDate(){
+      // se formatea la fecha de las peliculas usando un formato
+      // en este caso utiliza el formato de fecha/hora local
+      // dado que se usa .toLocaleDateString y se establece
+      // la configuracion regional "es-ES" y se le da el formato
+      // options (dia:numerico, mes:texto, año:numerico)
+      // ejemplo (11 julio de 1998)
+      const options = {day:'numeric', month:'long', year:'numeric'}
+      const release = new Date(this.movie.release_date)
+      return release.toLocaleDateString("es-ES",options)
+    },
+    formatRevenue(){
+      return this.dotsNumbers(this.movie.revenue)
+    },
+    formatBudget(){
+      return this.dotsNumbers(this.movie.budget)
+    },
+    formatVoteAverage(){
+      return (this.movie.vote_average * 10).toFixed(1)
+    }
+  },// fin de computed
 
-})
-.catch (error => console.log ("error:",error))
+//el methods similar al functions, se arman las funciones para ejecutar mas tarde
+  methods:{
+    dotsNumbers(number){
+      // se verifica que los datos sean distinto de undefined y null
+      // luego se usa el metodo toLocaleString que convierte
+      // un string segun la region establecida, se le da
+      // el stylo de moneda y se selecciona USD como moneda
+      if(number !== undefined && number !== null){
+        return number.toLocaleString("en-US",{
+          style: "currency",
+          currency: "USD",
+          minimumFractingDigits: 0,
+        })
+      }
+    }
+  }, //fin de method
+} // aca termina el options
+const app = createApp(options)
+app.mount ('#app')
