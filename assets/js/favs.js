df8b44e91649a81cb0cb59c93442d58b6b643354
fedeval98 <fede.val9806@gmail.com>
2023-11-22 08:51:49 -0300

    terminado seccion favs

--- assets/js/favs.js
+++ assets/js/favs.js
@@ -14,6 +14,9 @@ const options = {
       search: "",
       select: "none",
       movieFilter:[],
+      likes:[],
+      favMovies:[],
+      favFiltered:[],
     } //aca termina el return del data
   }, //aca termina el data
 
@@ -24,6 +27,9 @@ const options = {
         this.movies = data.movies
         this.movieFilter = this.movies
         this.genres = [...new Set(this.movies.map(movie => movie.genres).flat())]
+        this.favMovies = JSON.parse(localStorage.getItem('likes'))
+        this.favFiltered = this.movies.filter(movie =>this.favMovies.some(favMovie => favMovie.id === movie.id))
+        console.log(this.favFiltered)
       }) 
       .catch(error => console.log(`Error:${error}`))
   }, //aca termina el beforeCreate
@@ -41,103 +47,33 @@ const options = {
 
     filter(){
       this.movieFilter = this.movies.filter (movie => movie.title.toLowerCase().includes(this.search.toLowerCase()) && (this.select === 'none' || movie.genres.includes(this.select)))
-      console.log(this.movieFilter)
     }, //aca termina el filter
-    clearFilter(){
-      this.movieFilter = this.movies
-      this.search = ""
-      this.select = "none"
-      this.$forceUpdate()
-    },
-  }
+    
+    getFavImg(movie){
+      let localStorageMovie = JSON.parse(localStorage.getItem('likes')) || []
+      const movieLiked = localStorageMovie.some( item => item.id === movie.id)
+      return movieLiked ? '../img/like_fill.png' 
+                        : '../img/like.png'
+    }, //aca termina getFavImg
 
+    removeFavImg(movie){
+      let localStorageMovie = JSON.parse(localStorage.getItem('likes')) || []
+      const movieLiked = localStorageMovie.some( item => item.id === movie.id)
+      const imgRef = this.$refs['img_'+movie.id]
+      console.log(imgRef)
+        if (movieLiked){
+        localStorageMovie = localStorageMovie.filter(item => item.id !== movie.id)
+        imgRef.src = '../img/like.png'
+        this.$forceUpdate()
+        }
+      localStorage.setItem('likes', JSON.stringify(localStorageMovie))
+      this.favMovies = JSON.parse(localStorage.getItem('likes'))
+      this.favFiltered = this.movies.filter(movie =>this.favMovies.some(favMovie => favMovie.id === movie.id))
+      console.log(this.favFiltered)
+    }, //aca termina el changeFavImg
+
+}, // aca termina el methods
 
 } //aca termina el options
 const app = createApp(options)
 app.mount ('#app')
-
-
-/* //Captura de elementos del HTML
-const selectGenres = document.getElementById("selectgenres")
-const moviesContenedor = document.getElementById("divMovies")
-const searchInput = document.getElementById("searchInput")
-const buttonClear = document.getElementById("clear")
-
-//Importando las variables para ejecutar la funcion
-import {
-  introducirCard, 
-  crearOptions, 
-  crearElementosDelCard,
-  destructureMovies,
-  genresList,
-  manejarCambioSelect,
-  sortArray,
-  limpiarContenedor,
-}from '../js/module/functions.js'
-//Key de la API por headers
-const requestOptions = {
-  headers:{
-    "x-api-key": '0ff70d54-dc0b-4262-9c3d-776cb0f34dbd'
-  }
-}
-let moviesData = {}
-fetch ("https://moviestack.onrender.com/api/movies", requestOptions)
-  .then(response => response.json())
-  .then (data => {
-    moviesData = data
-    const movie = moviesData.movies
-
-    const movies = sortArray(movie)
-
-    let favMovies = JSON.parse(localStorage.getItem('likes'))
-    
-    console.log (Object.keys(localStorage).length)
-    if(favMovies.length == 0 || Object.keys(localStorage).length === 0){
-      moviesContenedor.className = ""
-      moviesContenedor.classList.add ("text-center", "items-center","w-full")
-      moviesContenedor.innerHTML = "NO FAVORITE MOVIES FOUND"
-    } else if (Object.keys(localStorage).length !== 0 || favMovies.length !== 0){
-
-      let favMoviesFiltered = movies.filter(movie =>favMovies.some(favMovie => favMovie.id === movie.id))
-      
-      introducirCard(favMoviesFiltered, moviesContenedor, crearElementosDelCard)
-
-      const listOfGenres = genresList(destructureMovies(favMoviesFiltered))
-      
-      crearOptions(listOfGenres, selectGenres)
-
-    moviesContenedor.addEventListener ('click', (event)=>{
-      const target = event.target
-
-      if (target.classList.contains("picture")){
-        favMovies = JSON.parse(localStorage.getItem('likes'))
-        favMoviesFiltered = movies.filter(movie =>favMovies.some(favMovie => favMovie.id === movie.id))
-        limpiarContenedor(moviesContenedor)
-        introducirCard(favMoviesFiltered, moviesContenedor, crearElementosDelCard)
-        const listOfGenres = genresList(destructureMovies(favMoviesFiltered))
-        crearOptions(listOfGenres, selectGenres)
-      }
-      if (favMovies.length == 0 || Object.keys(localStorage).length === 0){
-        moviesContenedor.className = ""
-        moviesContenedor.classList.add ("text-center", "items-center","w-full")
-        moviesContenedor.innerHTML = "NO FAVORITE MOVIES FOUND"
-      }
-    })
-
-      selectGenres.addEventListener('change', () => {
-        manejarCambioSelect(favMoviesFiltered, selectGenres, searchInput, moviesContenedor)
-      })
-      
-      searchInput.addEventListener('keyup', () => {
-          manejarCambioSelect(favMoviesFiltered, selectGenres, searchInput, moviesContenedor)
-        })
-      
-      buttonClear.addEventListener('click',()=>{
-        selectGenres.value = ""
-        searchInput.value = ""
-        manejarCambioSelect(favMoviesFiltered, selectGenres, searchInput, moviesContenedor)
-      })
-    }
-    
-  })
-  .catch (error => console.log ("error:",error)) */
